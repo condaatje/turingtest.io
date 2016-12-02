c32a9442f693c4fa27f538b8c37445c6d26def98
condaatje <christian.ondaatje@gmail.com>
2016-12-02 22:12:16 +0000

    got sockets working with a node server.

--- turingtest/ttio/static/ttio/js/chat.js
+++
@@ -1,2 +0,0 @@
-var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-var chat_socket = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
